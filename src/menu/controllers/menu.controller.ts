184e60d0098e2ae0415bf73a3c0adb46e5da6220
mfarel <mfarel@openpass.com.ar>
2024-05-28 21:28:05 -0300

    slint config

--- src/menu/controllers/menu.controller.ts
+++ src/menu/controllers/menu.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateMenuDto } from '../dtos/menu.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.auth.gards';
@@ -8,24 +17,23 @@ import { CreateMenuItemDto } from '../dtos/menu-item.dto';
 @ApiTags('menú ')
 @Controller('menu')
 export class MenuController {
-    constructor(private readonly menuService: MenuService) { }
+  constructor(private readonly menuService: MenuService) {}
 
-    @Get('/bydining/:id')
-    async getMenuByDining(@Param('id') id: string) {
-        return this.menuService.findMenuItemsByMenuId(id);
-    }
+  @Get('/bydining/:id')
+  async getMenuByDining(@Param('id') id: string) {
+    return this.menuService.findMenuItemsByMenuId(id);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post('/create/')
-    async createMenuwithItem(@Req() req: Request, @Body() menu: CreateMenuDto) {
-        var userID = req['user']['userId'];
-        return this.menuService.createMenu(menu, userID);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Post('/create/')
+  async createMenuwithItem(@Req() req: Request, @Body() menu: CreateMenuDto) {
+    const userID = req['user']['userId'];
+    return this.menuService.createMenu(menu, userID);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Put('/edit/')
-    async editMenuwithItem(@Req() req: Request, @Body() item: CreateMenuItemDto) {
-        var userID = req['user']['userId'];
-        return this.menuService.editItemsFromMenu(item);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Put('/edit/')
+  async editMenuwithItem(@Req() req: Request, @Body() item: CreateMenuItemDto) {
+    return this.menuService.editItemsFromMenu(item);
+  }
 }
